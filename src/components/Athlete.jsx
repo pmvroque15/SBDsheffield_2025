296c7f7894411e7b7e22060488950b04d2f2673f
Pat <pmvroque@gmail.com>
2024-07-27 13:46:05 -0400

    publish domain

--- src/components/Athlete.jsx
+++ src/components/Athlete.jsx
@@ -2,11 +2,14 @@ import Athletes from './Athletes'
 
 const Athlete = ({ athlete }) => {
   return (
-    <div>
+    <div id="athletes-info">
       <img src={athlete.profileImage} alt={athlete.firstName} />
       <h3>
-        {athlete.firstName} {athlete.lastName}
+        <a id="athlete-link" href={athlete.link}>
+          {athlete.firstName} {athlete.lastName}
+        </a>
       </h3>
+      <p>{athlete.weightClass} kg</p>
       <p id="nationality">{athlete.nationality}</p>
     </div>
   )
